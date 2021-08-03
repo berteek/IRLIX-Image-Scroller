85d140abe707790ca615f5492e7e74f41ee1351d
berteek <merzinalbert@gmail.com>
2021-08-03 14:57:34 +0400

    Project created

---
+++ app/src/main/java/com/example/irlix_image_scroller/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.irlix_image_scroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
